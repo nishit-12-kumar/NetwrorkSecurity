4c99f26314291eebefcf49dcaf379dd7e35a794b
Nishit Kumar <nishitkumaroll12@gmail.com>
2025-10-18 06:45:49 +0530

    Data Validation Completed

--- NetworkSecurity/constant/training_pipeline/__init__.py
+++ NetworkSecurity/constant/training_pipeline/__init__.py
@@ -3,8 +3,9 @@ import sys
 import pandas as pd
 import numpy as np
 
+
 """
-defining common constant variable for training pipeline
+defining common constant variable for training pipeline...
 """
 TARGET_COLUMN = "Result"
 PIPELINE_NAME: str = "NetworkSecurity"
@@ -14,9 +15,12 @@ FILE_NAME: str = "phisingData.csv"
 TRAIN_FILE_NAME: str = "train.csv"
 TEST_FILE_NAME: str = "test.csv"
 
+SCHEMA_FILE_PATH = os.path.join("data_schema" , "schema.yaml")
+
+
 
 """
-Data Ingestion related constant start with DATA_INGESTION VAR NAME
+Data Ingestion related constant start with DATA_INGESTION VAR NAME...
 """
 DATA_INGESTION_COLLECTION_NAME: str = "NetworkData"
 DATA_INGESTION_DATABASE_NAME: str = "NishitAI"
@@ -24,3 +28,14 @@ DATA_INGESTION_DIR_NAME: str = "data_ingestion"
 DATA_INGESTION_FEATURE_STORE_DIR: str = "feature_store"
 DATA_INGESTION_INGESTED_DIR: str = "ingested"
 DATA_INGESTION_TRAIN_TEST_SPLIT_RATION: float = 0.2
+
+
+
+"""
+Data Validation related constant start with DATA_VALIDATION VAR NAME...
+"""
+DATA_VALIDATION_DIR_NAME: str = "data_validation"
+DATA_VALIDATION_VALID_DIR: str = "validated"
+DATA_VALIDATION_INVALID_DIR: str = "invalid"
+DATA_VALIDATION_DRIFT_REPORT_DIR: str = "drift_report"
+DATA_VALIDATION_DRIFT_REPORT_FILE_NAME: str = "report.yaml"
--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from NetworkSecurity.components.data_ingestion import DataIngestion
 from NetworkSecurity.exception.exception import NetworkSecurityException
 from NetworkSecurity.logging.logger import logging
-from NetworkSecurity.entity.config_entity import DataIngestionConfig 
+from NetworkSecurity.entity.config_entity import DataIngestionConfig , DataValidationConfig
 from NetworkSecurity.entity.config_entity import TrainingPipelineConfig 
+from NetworkSecurity.components.data_validation import DataValidation
 
 import sys
 
@@ -10,13 +11,26 @@ import sys
 if __name__ == "__main__":
     try:
         trainingpipelineconfig = TrainingPipelineConfig()
+
         dataingestionconfig = DataIngestionConfig(trainingpipelineconfig)
         data_ingestion = DataIngestion(dataingestionconfig)
 
-        logging.info("Initiate the data ingestion")
+        logging.info("Initiate the data ingestion...")
 
         dataingestionartifact = data_ingestion.initiate_data_ingestion()
-        print(dataingestionartifact )
+
+        logging.info("Data Inititation Completed.")
+        print(dataingestionartifact)
+
+        datavalidationconfig = DataValidationConfig(trainingpipelineconfig)
+        data_validation =  DataValidation(dataingestionartifact , datavalidationconfig)
+
+        logging.info("Initiate the Data Validation...")
+
+        datavalidationartifact = data_validation.initiate_data_validation()
+
+        logging.info("Data Validation Completed...")         
+        print(datavalidationartifact)
 
 
     except Exception as e:
